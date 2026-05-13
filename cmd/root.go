c9ab90f847ccaa8dbdedcf48a076c306fe24f879
goveiajoao <joaoantoniogoveia@outlook.com>
2026-05-13 11:59:49 -0300

    add short and long comment in root

--- cmd/root.go
+++ cmd/root.go
@@ -10,16 +10,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "dfctl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A dotfiles manager",
+	Long: `Used to manage your dotfiler from a github repo`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
