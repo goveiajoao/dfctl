e56614834510c4c30c4c4b2062ac9e6a63edd443
goveiajoao <joaoantoniogoveia@outlook.com>
2026-05-15 14:58:39 -0300

    add root flags

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -7,15 +7,12 @@ import (
 
 
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "dfctl",
 	Short: "A dotfiles manager",
 	Long: `Used to manage your dotfiler from a github repo`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -23,16 +20,15 @@ func Execute() {
 	}
 }
 
+// Root Flags 
+var (
+    NoConfirm 	bool
+    AutoPull 	bool
+    AutoPush 	bool
+)
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.dfctl.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+    rootCmd.Flags().BoolVar(&NoConfirm, "noconfirm", false, "Remove confirmations")
+    rootCmd.Flags().BoolVar(&AutoPull, "autopull", true, "Auto pull before any action")
+    rootCmd.Flags().BoolVar(&AutoPush, "autopush", true, "Auto push after any action")
+    rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
